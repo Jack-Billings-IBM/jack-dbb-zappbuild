f8df8e25f1e95a0c2ab9d48571aa070cf8939a92
Jack Billings <jack.billings@ibm.com>
2024-01-04 08:56:21 -0600

    added dbb repo scripts

---
+++ Pipeline/PackageBuildOutputs/ArtifactRepositoryHelpers.groovy
@@ -0,0 +1,184 @@
+@groovy.transform.BaseScript com.ibm.dbb.groovy.ScriptLoader baseScript
+import groovy.transform.*
+import javax.net.ssl.SSLContext
+import groovy.cli.commons.*
+
+import java.net.http.*
+import java.net.http.HttpRequest.BodyPublishers
+import java.net.http.HttpResponse.BodyHandlers
+import java.net.http.HttpResponse.BodyHandler
+import java.util.concurrent.CompletableFuture
+
+import java.nio.file.Paths
+
+
+/** Very basic script to upload/download from an artifact repository server
+ *
+ * Version 1 - 2022
+ * 
+ * This script requires JAVA 11 because it uses java.net.http.* APIs to create
+ * the HTTPClient and HTTPRequest, replacing the previous ArtifactoryHelper
+ *
+ * Version 2 - 2023-04
+ * 
+ * Implemented a retry mechanism for upload and download (with Connection Keep-alive for upload)
+ */
+
+@Field int MAX_RESEND = 10;
+
+def <T> CompletableFuture<HttpResponse<T>>
+		tryResend(HttpClient client, HttpRequest request, BodyHandler<T> handler,
+				 int count, HttpResponse<T> resp) {
+	if (resp.statusCode() == 200 || count >= MAX_RESEND) {
+		return CompletableFuture.completedFuture(resp);
+	} else {
+		return client.sendAsync(request, handler)
+				.thenComposeAsync(r -> tryResend(client, request, handler, count+1, r));
+	}
+}
+
+run(args)
+
+def upload(String url, String fileName, String user, String password, boolean verbose) throws IOException {
+	System.setProperty("jdk.httpclient.allowRestrictedHeaders", "Connection")
+    println( "** ArtifactRepositoryHelper started for upload of $fileName to $url" );
+    
+    // create http client
+    HttpClient.Builder httpClientBuilder = HttpClient.newBuilder()
+    .authenticator(new Authenticator() {
+        @Override
+        protected PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication(
+            "$user",
+            "$password".toCharArray());
+        }
+    });
+
+//  if ( disableSSLVerify ) {
+//      SSLContext sc = SSLContext.getInstance(DEFAULT_SSL_PROTOCOLS);
+//      sc.init(null, trustAllCertsTrustManager(), null);
+//      httpClientBuilder.sslContext(sc)
+//  }
+
+    HttpClient httpClient = httpClientBuilder.build();
+    
+    // build http request
+    HttpRequest request = HttpRequest.newBuilder()
+    .uri(URI.create("$url"))
+    .header("Content-Type", "binary/octet-stream")
+	.header("Connection","Keep-Alive")
+    .PUT(BodyPublishers.ofFile(Paths.get(fileName)))
+    .build();
+
+	println("** Uploading $fileName to $url...");
+	
+	HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+    // submit request
+	CompletableFuture<HttpResponse<String>> response = httpClient.sendAsync(request, handler).thenComposeAsync(r -> tryResend(httpClient, request, handler, 1, r));
+	HttpResponse finalResponse = response.get()
+    
+    if (verbose)
+		println("** Response: " + finalResponse);
+    
+    def rc = evaluateHttpResponse(finalResponse, "upload", verbose)
+    
+    if (rc == 0 ) {
+        println("** Upload completed.");
+    }
+    else {
+        println("*! Upload failed.");
+    }
+}
+
+def download(String url, String fileName, String user, String password, boolean verbose) throws IOException  {
+    println("** ArtifactRepositoryHelper started for download of $url to $fileName.");
+
+    // create http client
+    HttpClient.Builder httpClientBuilder = HttpClient.newBuilder()
+    .authenticator(new Authenticator() {
+        @Override
+        protected PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication(
+            "$user",
+            "$password".toCharArray());
+        }
+    });
+
+//  if ( disableSSLVerify ) {
+//      SSLContext sc = SSLContext.getInstance(DEFAULT_SSL_PROTOCOLS);
+//      sc.init(null, trustAllCertsTrustManager(), null);
+//      httpClientBuilder.sslContext(sc)
+//  }
+
+    HttpClient httpClient = httpClientBuilder.build();
+    
+    // build http request
+    HttpRequest request = HttpRequest.newBuilder()
+    .uri(URI.create("$url"))
+    .GET()
+    .build();
+    
+    // submit request
+    println("** Downloading $url to $fileName...");
+	HttpResponse.BodyHandler<InputStream> handler = HttpResponse.BodyHandlers.ofInputStream();
+	CompletableFuture<HttpResponse<String>> response = httpClient.sendAsync(request, handler).thenComposeAsync(r -> tryResend(httpClient, request, handler, 1, r));
+
+	HttpResponse finalResponse = response.get()
+	
+    // evalulate response
+    rc = evaluateHttpResponse(finalResponse, "download", verbose)
+    
+    if (rc == 0) {
+        // write file to output 
+        def responseBody = finalResponse.body()
+        println("** Writing to file to $fileName.")
+        FileOutputStream fos = new FileOutputStream(fileName);
+        fos.write(responseBody.readAllBytes());
+        fos.close();
+    } else {
+        println("*! Download failed.");
+    }
+}
+
+def evaluateHttpResponse (HttpResponse response, String action, boolean verbose) {
+    int rc = 0
+    def statusCode = response.statusCode()
+    if (verbose) println "*** HTTP-Status Code: $statusCode"
+    def responseString = response.body()
+    if ((statusCode != 201) && (statusCode != 200)) {
+        rc = 1
+        println("** Artifactory $action failed with statusCode : $statusCode")
+        println("** Response: " + response);
+        throw new RuntimeException("Exception : Artifactory $action failed:" + statusCode);
+    }    
+    return rc
+}
+
+//Parsing the command line
+def run(String[] cliArgs) {
+    def cli = new CliBuilder(usage: "ArtifactRepositoryHelpers.groovy [options]", header: '', stopAtNonOption: false)
+    cli.h(longOpt:'help', 'Prints this message')
+    cli.u(longOpt:'url', args:1, required:true, 'Artifactory file uri location')
+    cli.fU(longOpt:'fileToUpload', args:1, 'The full path of the file to upload')
+    cli.fD(longOpt:'fileToDownload', args:1, 'The full path of the file to download')
+    cli.U(longOpt:'user', args:1, required:true, 'Artifactory user id')
+    cli.P(longOpt:'password', args:1, required:true, 'Artifactory password')
+    cli.v(longOpt:'verbose', 'Flag to turn on script trace')
+    def opts = cli.parse(cliArgs)
+
+    // if opt parsing fails, exit
+    if (opts == null || !opts) {
+        System.exit(1)
+    }
+
+    if (opts.h) {
+        cli.usage()
+        System.exit(0)
+    }
+    
+    if ( opts.fU) {
+        upload(opts.u, opts.fU, opts.U, opts.P, opts.v)
+    } else {
+        download(opts.u, opts.fD, opts.U, opts.P, opts.v)
+    }
+}
---
+++ Templates/Common-Backend-Scripts/utilities/dbbBuildUtils.sh
@@ -0,0 +1,229 @@
+# internal veriables
+mainBranchSegment=""
+secondBranchSegment=""
+baselineReferenceFile=""
+segmentName=""
+
+computeBuildConfiguration() {
+
+    ##DEBUG ## echo -e "App name \t: ${App}"
+    ##DEBUG ## echo -e "Branch name \t: ${Branch}"
+
+    # reset variables
+    propOverrides=""
+
+    # Compute HLQ preix and application name
+    HLQ=$(echo ${HLQPrefix}.${App:0:8} | tr '[:lower:]' '[:upper:]' | tr -d '-')
+
+    # Locate the baseline reference file based on the baselineReferenceLocation config in pipelineBackend.config
+    baselineReferenceFile="${AppDir}/$baselineReferenceLocation"
+
+    if [ ! -f "${baselineReferenceFile}" ]; then
+        rc=8
+        ERRMSG=$PGM": [ERROR] Applications baseline reference configuration file (${baselineReferenceFile}) was not found. rc="$rc
+        echo $ERRMSG
+    fi
+
+    branchConvention=(${Branch//// })
+
+    if [ $rc -eq 0 ]; then
+        if [ ${#branchConvention[@]} -gt 3 ]; then
+            rc=8
+            ERRMSG=$PGM": [ERROR] Script is only managing branch name with up to 3 segments (${Branch}) . rc="$rc
+            echo $ERRMSG
+        fi
+    fi
+
+    if [ $rc -eq 0 ]; then
+
+        # split the segments
+        mainBranchSegment=$(echo ${Branch} | awk -F "/" ' { print $1 }')
+        secondBranchSegment=$(echo ${Branch} | awk -F "/" ' { print $2 }')
+        thirdBranchSegment=$(echo ${Branch} | awk -F "/" ' { print $3 }')
+
+        # remove chars (. -) from the name
+        mainBranchSegmentTrimmed=$(echo ${mainBranchSegment} | tr -d '.-' | tr '[:lower:]' '[:upper:]')
+        
+        # evaluate main segment
+        case $mainBranchSegmentTrimmed in
+        REL* | EPIC* | PROJ*)
+            # Release maintenance, epic and project branches are intergration branches,
+            # that derive dependency information from mainBuildBranch configuration.
+
+            # evaluate third segment
+            if [ ! -z "${thirdBranchSegment}" ]; then
+                rc=8
+                ERRMSG=$PGM": [ERROR] Branch (${Branch}) does not follow standard naming conventions  . rc="$rc
+                echo $ERRMSG
+            else
+                # feature branches for next planned release
+                computeSegmentName $secondBranchSegment
+                HLQ="${HLQ}.${mainBranchSegmentTrimmed:0:1}${segmentName:0:7}"
+            fi
+
+            getBaselineReference
+            if [ -z "${Type}" ]; then
+                Type="--impactBuild"
+                Type="${Type} --baselineRef ${baselineRef}"
+                # Release maintenance / epic / project branch clones the dependency information from the main build branch
+                # propOverrides="mainBuildBranch=${mainBranchSegment}"
+                # appending the --debug flag to compile with TEST options
+                Type="${Type} --debug"
+            fi
+            ;;
+        FEATURE*)
+            # all feature branche start with F
+
+            # evaluate third segment
+            if [ ! -z "${thirdBranchSegment}" ]; then
+                # feature branches for EPIC workflow
+                
+                # // skipped for simplicity
+                # computeSegmentName $secondBranchSegment
+                # HLQ="${HLQ}.E${segmentName:0:7}"
+                computeSegmentName $thirdBranchSegment
+                HLQ="${HLQ}.F${segmentName:0:7}"
+                
+                propOverrides="mainBuildBranch=epic/${secondBranchSegment}"
+
+            else
+                # feature branches for next planned release
+                computeSegmentName $secondBranchSegment
+                HLQ="${HLQ}.F${segmentName:0:7}"
+            fi
+
+            if [ -z "${Type}" ]; then
+                Type="--impactBuild"
+                # appending the --debug flag to compile with TEST options
+                Type="${Type} --debug"
+            fi
+            ;;
+        HOTFIX*)
+
+            # evaluate third segment
+            if [ ! -z "${thirdBranchSegment}" ]; then
+                # feature branches for hotfix workflow
+
+                # // skipped for simplicity
+                # computeSegmentName $secondBranchSegment
+                # HLQ="${HLQ}.R${segmentName:0:7}"
+                computeSegmentName $thirdBranchSegment
+                HLQ="${HLQ}.H${segmentName:0:7}"
+            else
+                rc=8
+                ERRMSG=$PGM": [ERROR] Hotfix branch (${Branch}) does not follow naming conventions  . rc="$rc
+                echo $ERRMSG
+            fi
+
+            if [ -z "${Type}" ]; then
+                Type="--impactBuild"
+                propOverrides="mainBuildBranch=release/${secondBranchSegment}"
+            fi
+            ;;
+        "PROD" | "MASTER" | "MAIN")
+            getBaselineReference
+            if [ -z "${Type}" ]; then
+                Type="--impactBuild"
+                Type="${Type} --baselineRef ${baselineRef}"
+            fi
+            if [ "${PipelineType}" == "release" ]; then
+                HLQ="${HLQ}.${mainBranchSegmentTrimmed:0:8}.REL"
+            else
+                HLQ="${HLQ}.${mainBranchSegmentTrimmed:0:8}.BLD"
+                if [ -z "${Type}" ]; then
+                    # appending the --debug flag to compile with TEST options
+                    Type="${Type} --debug"
+                fi
+            fi
+
+            ;;
+        *)
+            # Treat other branches as feature branch.
+            # Assuming <initiative>/<feature> as naming convention
+            # Comment: We might want to verify that the epic branch exists.
+
+            HLQ="${HLQ}.${mainBranchSegmentTrimmed:0:8}"
+            if [ -z "${Type}" ]; then
+                Type="--impactBuild"
+                # appending the --debug flag to compile with TEST options
+                Type="${Type} --debug"
+            fi
+            # override the mainBuildBranch to clone the dependency collection for the initative.
+            propOverrides="mainBuildBranch=epic/${mainBranchSegment}"
+
+            ;;
+
+            # rc=8
+            # ERRMSG=$PGM": [ERROR] [Utilities/dbbBuildUtils.sh(computeBuildConfiguration())] No build conventions/rules for branch name (${Branch}) have been defined. rc="$rc
+            # echo $ERRMSG
+            # ;;
+        esac
+
+        # append pipeline preview if specified
+        if [ "${PipelineType}" == "preview" ]; then
+            if [ -z "${Type}" ]; then
+                Type="${Type} --preview"
+            fi
+        fi
+
+        # print computed values
+        ##DEBUG ## echo -e "Computed hlq \t: ${HLQ}"
+        ##DEBUG ## echo -e "Build option \t: ${Type}"
+
+        # unset variables
+        baselineRef=""
+        mainBranchSegment=""
+        mainBranchSegmentTrimmed=""
+        secondBranchSegment=""
+        secondBranchSegmentTrimmed=""
+        thirdBranchSegment=""
+        thirdBranchSegmentTrimmed=""
+        branchConvention=""
+        segmentName=""
+
+    fi
+
+}
+
+# Private method to retrieve the baseline reference from the configuration file
+
+getBaselineReference() {
+
+    baselineRef=$(cat "${baselineReferenceFile}" | grep "^${mainBranchSegment}" | awk -F "=" ' { print $2 }')
+
+    if [ -z "${baselineRef}" ]; then
+        rc=8
+        ERRMSG=$PGM": [ERROR] No baseline ref was found for branch name ${Branch} in ${baselineReferenceFile}. rc="$rc
+        echo $ERRMSG
+    fi
+
+    ##DEBUG ## echo -e "baselineRef \t: ${baselineRef}"    ## DEBUG
+}
+
+
+#
+# computation of branch segments
+# captured cases
+#
+# containing numbers, assuming to be an work-item-id
+# containing strings and words separated by dashes, return first characters of each string
+# none of the above - return segement name in upper case w/o underscores
+#
+
+computeSegmentName() {
+
+    segmentName=$1
+    echo $segmentName
+    if [ ! -z $(echo "$segmentName" | tr -dc '0-9') ]; then
+        # "contains numbers"
+        retval=$(echo "$segmentName" | tr -dc '0-9')
+    elif [[ $segmentName == *"-"* ]]; then
+        # contains dashes
+        segmentNameTrimmed=$(echo "$segmentName" | awk -F "-" '{ for(i=1; i <= NF;i++) print($i) }' | cut -c-1-1)
+        segment1=$(echo "$segmentNameTrimmed" | tr -d '\n')
+        retval=$(echo "$segment1" | tr '[:lower:]' '[:upper:]')
+    else 
+        retval=$(echo "$segmentName" | tr -d '_' | tr '[:lower:]' '[:upper:]')
+    fi
+    segmentName=$(echo "$retval")
+}
